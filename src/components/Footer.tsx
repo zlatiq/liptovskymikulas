8d06d109db0023111f2fe19ce970f73f2167e8c3
Zlatica Štrkolcová <strkolcovazlatica@gmail.com>
2025-01-28 23:05:05 +0100

    Mikulas web hotovo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FaFacebook, FaInstagram, FaTwitter, FaEnvelope } from 'react-icons/fa';
+import logo from '../assets/logo.png'; // Import loga
 
 const FooterWrapper = styled.footer`
-    background-color: #1466AC;
-    color: white;
-    padding: 2rem 0;
+  background-color: #1466AC;
+  color: white;
+  padding: 2rem 0;
 `;
 
 const FooterContent = styled.div`
@@ -18,7 +19,15 @@ const FooterContent = styled.div`
 `;
 
 const FooterInfo = styled.div`
-    margin-bottom: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 2.5rem;
+`;
+
+const LogoImage = styled.img`
+    height: 120px;
+    width: auto;
+    filter: brightness(0) invert(1); // Pre biele logo na modrom pozadí
 `;
 
 const SocialLinks = styled.div`
@@ -37,30 +46,33 @@ const SocialLink = styled.a`
 
 const Footer: React.FC = () => {
     return (
-                <FooterWrapper>
-                    <FooterContent>
-                        <FooterInfo>
-                            <h3>Mesto Liptovský Mikuláš</h3>
-                            <p>Štúrova 1989/41, 031 42 Liptovský Mikuláš</p>
-                            <p>Tel: +421 44 556 5111</p>
-                        </FooterInfo>
-                        <SocialLinks>
-                            <SocialLink href="https://www.facebook.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
-                                <FaFacebook />
-                            </SocialLink>
-                            <SocialLink href="https://www.instagram.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
-                                <FaInstagram />
-                            </SocialLink>
-                            <SocialLink href="https://twitter.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
-                                <FaTwitter />
-                            </SocialLink>
-                            <SocialLink href="mailto:info@mikulas.sk">
-                                <FaEnvelope />
-                            </SocialLink>
-                        </SocialLinks>
-                    </FooterContent>
-                </FooterWrapper>
-                );
-                };
+        <FooterWrapper>
+            <FooterContent>
+                <FooterInfo>
+                    <LogoImage src={logo} alt="Logo mesta Liptovský Mikuláš" />
+                    <div>
+                        <h3>Mesto Liptovský Mikuláš</h3>
+                        <p>Štúrova 1989/41, 031 42 Liptovský Mikuláš</p>
+                        <p>Tel: +421 44 556 5111</p>
+                    </div>
+                </FooterInfo>
+                <SocialLinks>
+                    <SocialLink href="https://www.facebook.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook />
+                    </SocialLink>
+                    <SocialLink href="https://www.instagram.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram />
+                    </SocialLink>
+                    <SocialLink href="https://twitter.com/liptovskymikulas" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter />
+                    </SocialLink>
+                    <SocialLink href="mailto:info@mikulas.sk">
+                        <FaEnvelope />
+                    </SocialLink>
+                </SocialLinks>
+            </FooterContent>
+        </FooterWrapper>
+    );
+};
 
-                export default Footer;
+export default Footer;
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,4 @@
-import type React from "react"
+import React from "react"
 import styled from "styled-components"
 import { Carousel } from "react-responsive-carousel"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
@@ -10,46 +10,61 @@ import image3 from "../assets/gallery/3.jpg"
 import image4 from "../assets/gallery/4.jpg"
 
 const GalleryWrapper = styled.section`
-  padding: 4rem 0;
-  background-color: #f8f9fa;
+    padding: 4rem 0;
+    background-color: #f8f9fa;
 `
 
 const GalleryContent = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
+    max-width: 800px; // Zmenšená šírka galérie
+    margin: 0 auto;
 `
 
 const GalleryTitle = styled.h2`
-  font-size: 2.5rem;
-  margin-bottom: 2rem;
-  text-align: center;
-  color: #1466AC;
+    font-size: 2.5rem;
+    margin-bottom: 2rem;
+    text-align: center;
+    color: #1466AC;
 `
 
 const StyledCarousel = styled(Carousel)`
-  .carousel .slide img {
-    max-height: 600px;
-    object-fit: cover;
-  }
+    .carousel .slide img {
+        max-height: 400px; // Zmenšená výška obrázkov
+        object-fit: cover;
+    }
+
+    .legend {
+        background: rgba(20, 102, 172, 0.8);
+        font-size: 1rem;
+        padding: 10px;
+        margin-bottom: 40px;
+        border-radius: 10px;
+    }
 `
 
 const Gallery: React.FC = () => {
     const images = [
-        { src: image1, alt: "Námestie osloboditeľov v Liptovskom Mikuláši", legend: "Kostol sv. Mikuláša" },
-        { src: image2, alt: "Kostol sv. Mikuláša", legend: "Budova spolku Tatrín" },
-        { src: image3, alt: "Aquapark Tatralandia", legend: "Nízke Tatry - Chopok" },
-        { src: image4, alt: "Panoráma Chopku", legend: "Liptovská Mara" },
+        { src: image1, alt: "Námestie osloboditeľov", legend: "Historické námestie s Kostolom sv. Mikuláša v pozadí" },
+        { src: image2, alt: "Kostol sv. Mikuláša", legend: "Gotický Kostol sv. Mikuláša - symbol mesta" },
+        { src: image3, alt: "Aquapark Tatralandia", legend: "Zábava a relax v Aquaparku Tatralandia" },
+        { src: image4, alt: "Chopok", legend: "Majestátny výhľad na Chopok v Nízkych Tatrách" },
     ]
 
     return (
         <GalleryWrapper id="gallery">
             <GalleryContent>
                 <GalleryTitle>Galéria</GalleryTitle>
-                <StyledCarousel showArrows={true} showThumbs={false} infiniteLoop={true} autoPlay={true} interval={5000}>
+                <StyledCarousel
+                    showArrows={true}
+                    showThumbs={false}
+                    infiniteLoop={true}
+                    autoPlay={true}
+                    interval={5000}
+                    showStatus={false}
+                >
                     {images.map((image, index) => (
                         <div key={index}>
                             <img src={image.src || "/placeholder.svg"} alt={image.alt} />
-                            <p className="legend">{image.legend}</p>
+
                         </div>
                     ))}
                 </StyledCarousel>
@@ -59,4 +74,3 @@ const Gallery: React.FC = () => {
 }
 
 export default Gallery
-
